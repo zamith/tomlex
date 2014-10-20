c6e851985c60d23f93d3fa89f9504d2ec867628f
Zamith <zamith.28@gmail.com>
2014-10-20 13:07:18 -0500

    Refactors parser

--- lib/tomlex.ex
+++ lib/tomlex.ex
@@ -5,11 +5,22 @@ defmodule Tomlex do
   end
 
   defp parse(line, result) do
+    parse_assignment(line, result)
+  end
+
+  defp parse_assignment(line, result) do
     [key, value] = String.split(line, "=", trim: true, parts: 2)
+    case parse_value(value) do
+      {:ok, parsed_value} -> Map.put result, String.to_atom(String.strip(key)), parsed_value
+      :error -> :error
+    end
+  end
+
+  defp parse_value(value) do
     parsed_value = String.strip(value) |> Integer.parse
     case parsed_value do
-      {int, _} -> Map.put result, String.to_atom(String.strip(key)), int
-      :error -> result
+      {int, _} -> {:ok, int}
+      :error -> :error
     end
   end
 end
